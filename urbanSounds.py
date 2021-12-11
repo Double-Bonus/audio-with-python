a58f9f88fb4cde55fa10632d2bd67ecf6844974f
Zygimantas <zygimantasmarma@gmail.com>
2021-12-11 22:54:48 +0200

    Basic example works

--- urbanSounds.py
+++ urbanSounds.py
@@ -36,29 +36,60 @@ import librosa.display
 import glob 
 import skimage
 
+
+def show_basic_data():
+    dat1, sampling_rate1 = librosa.load(BASE_PATH + "//audio//fold5//100032-3-0-0.wav")
+    dat2, sampling_rate2 = librosa.load(BASE_PATH + "//audio//fold5//100263-2-0-117.wav")
+    plt.figure(figsize=(20, 10))
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(dat1)), ref=np.max)
+    plt.subplot(4, 2, 1)
+    librosa.display.specshow(D, y_axis='linear')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Linear-frequency power spectrogram')
+    # plt.show()
+
+    # plt.figure(figsize=(20, 10))
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(dat2)), ref=np.max)
+    plt.subplot(4, 2, 2)
+    librosa.display.specshow(D, y_axis='linear')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Linear-frequency power spectrogram')
+    plt.show()
+
+
+
 DEBUG_MODE = 1
 BASE_PATH = "Urband_sounds//UrbanSound8K"
 
 
 df = pd.read_csv("Urband_sounds//UrbanSound8K//metadata//UrbanSound8K.csv")
-'''We will extract classes from this metadata.'''
 if DEBUG_MODE:
     print(df.head())
+    
+    show_basic_data()
 
 
 
 
-dat1, sampling_rate1 = librosa.load(BASE_PATH + "//audio//fold5//100032-3-0-0.wav")
-dat2, sampling_rate2 = librosa.load(BASE_PATH + "//audio//fold5//100263-2-0-117.wav")
-plt.figure(figsize=(20, 10))
-D = librosa.amplitude_to_db(np.abs(librosa.stft(dat1)), ref=np.max)
-plt.subplot(4, 2, 1)
-librosa.display.specshow(D, y_axis='linear')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Linear-frequency power spectrogram')
+
+'''Using random samples to observe difference in waveforms.'''
+
+arr = np.array(df["slice_file_name"])
+fold = np.array(df["fold"])
+cla = np.array(df["class"])
+
+j = 1
+plt.figure(figsize=(10, 5))
+for i in range(175, 197, 3):
+    path = BASE_PATH  + "//audio//fold" + str(fold[i]) + '//' + arr[i]
+    data, sampling_rate = librosa.load(path)
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(data)), ref=np.max)
+    plt.subplot(4, 2, j)
+    j = j + 1
+    librosa.display.specshow(D, y_axis='linear')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title(cla[i])
 plt.show()
-
-
 
 
 
