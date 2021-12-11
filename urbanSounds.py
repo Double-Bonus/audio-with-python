c3fe816f7d859f4214f85c1bfe80abca57361928
Zygimantas <zygimantasmarma@gmail.com>
2021-12-11 23:54:18 +0200

    Saves png with dif. lenght

--- urbanSounds.py
+++ urbanSounds.py
@@ -34,8 +34,7 @@ import os
 import librosa
 import librosa.display
 import glob 
-import skimage
-
+import skimage.io
 
 def show_basic_data():
     dat1, sampling_rate1 = librosa.load(BASE_PATH + "//audio//fold5//100032-3-0-0.wav")
@@ -56,10 +55,67 @@ def show_basic_data():
     plt.title('Linear-frequency power spectrogram')
     plt.show()
 
+def show_diff_classes():
+    '''Using random samples to observe difference in waveforms.'''
+    arr = np.array(df["slice_file_name"])
+    fold = np.array(df["fold"])
+    cla = np.array(df["class"])
+
+    j = 1
+    plt.figure(figsize=(10, 5))
+    for i in range(175, 197, 3):
+        path = BASE_PATH  + "//audio//fold" + str(fold[i]) + '//' + arr[i]
+        data, sampling_rate = librosa.load(path)
+        D = librosa.amplitude_to_db(np.abs(librosa.stft(data)), ref=np.max)
+        plt.subplot(4, 2, j)
+        j = j + 1
+        librosa.display.specshow(D, y_axis='linear')
+        plt.colorbar(format='%+2.0f dB')
+        plt.title(cla[i])
+    plt.show()
+
+# why ????
+def scale_minmax(X, min=0.0, max=1.0):
+    X_std = (X - X.min()) / (X.max() - X.min())
+    X_scaled = X_std * (max - min) + min
+    return X_scaled
+
+def spectrogram_image(y, sr, out, n_mels):
+    # use log-melspectrogram
+    mels = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels)
+    
+    
+    mels = np.log(mels + 1e-9) # add small number to avoid log(0)
+
+    # min-max scale to fit inside 8-bit range
+    img = scale_minmax(mels, 0, 255).astype(np.uint8)
+    img = np.flip(img, axis=0) # put low frequencies at the bottom in image
+    img = 255-img # invert. make black==more energy
+
+    # save as PNG
+    skimage.io.imsave(out, img)
+    
+    
+'''Saves data sound files as png pictures.'''
+def parser():
+    # Function to load files and extract features
+    for i in range(20):
+        file_name = BASE_PATH  + "//audio//fold" + str(df["fold"][i]) + '//' + df["slice_file_name"][i]
+        # Here kaiser_fast is a technique used for faster extraction
+        y, sr = librosa.load(file_name, res_type='kaiser_fast') 
+        
+        out = 'out' + str(i) + '.png' # TODO move out of root dir!!!!!!!!!!!!!!!!
+        n_mels = 128 # number of bins in spectrogram. Height of image
+        spectrogram_image(y, sr, out, n_mels)
+
+        label.append(df["classID"][i])
 
 
-DEBUG_MODE = 1
+
+
+DEBUG_MODE = 0
 BASE_PATH = "Urband_sounds//UrbanSound8K"
+DATA_SAMPLES_CNT = 8732
 
 
 df = pd.read_csv("Urband_sounds//UrbanSound8K//metadata//UrbanSound8K.csv")
@@ -67,31 +123,11 @@ if DEBUG_MODE:
     print(df.head())
     
     show_basic_data()
+    show_diff_classes()
 
-
-
-
-
-'''Using random samples to observe difference in waveforms.'''
-
-arr = np.array(df["slice_file_name"])
-fold = np.array(df["fold"])
-cla = np.array(df["class"])
-
-j = 1
-plt.figure(figsize=(10, 5))
-for i in range(175, 197, 3):
-    path = BASE_PATH  + "//audio//fold" + str(fold[i]) + '//' + arr[i]
-    data, sampling_rate = librosa.load(path)
-    D = librosa.amplitude_to_db(np.abs(librosa.stft(data)), ref=np.max)
-    plt.subplot(4, 2, j)
-    j = j + 1
-    librosa.display.specshow(D, y_axis='linear')
-    plt.colorbar(format='%+2.0f dB')
-    plt.title(cla[i])
-plt.show()
-
-
+# store y value to cnn
+label = []
+parser()
 
 
 print('Done!')
