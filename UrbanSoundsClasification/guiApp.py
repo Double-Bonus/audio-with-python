0db919610fd4a2bd28dc3f023c71ec32757ed714
Zygimantas <zygimantasmarma@gmail.com>
2022-05-22 23:58:40 +0300

    fix: corrections to gui

--- UrbanSoundsClasification/guiApp.py
+++ UrbanSoundsClasification/guiApp.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 
-
+CHECK_BOX_W = 16
+CAT_LABELS_W = 20
 
 #callbacks
 def Item_test():
@@ -19,8 +20,7 @@ def Item_test():
             
     print("\n")
 
-soundCategories = ["Gyvunai    ", "Aplinka ir vandens garsai    ",
-                   "Žmonių, nekalbiniai garsai    ", 	"Buitiniai garsai    ", 	"Miesto garsai    "]
+soundCategories = ["Gyvunų garsai", "Aplinkos ir vandens garsai", "Žmonių, nekalbiniai garsai", "Buitiniai garsai", "Miesto garsai"]
 
 animalsStr = [
 "Šuo",
@@ -54,7 +54,7 @@ humanNon_speechStr = [
 "Kvėpavimas",
 "Kosėjimas",
 "Žingsniai",
-"Juokiasi",
+"Juokas",
 "Dantų valymas",
 "Knarkimas",
 "Gerimas"
@@ -62,7 +62,7 @@ humanNon_speechStr = [
 
 buitiniaiStr = [
 "Durų beldimas",
-"Komp. Pelės paspaudimas",
+"Komp. pelės paspaudimas",
 "Rašymas klaviatūra",
 "Durų girgždėjimas",
 "Skardinės atidarymas",
@@ -89,7 +89,7 @@ miestoGarsaiStr = [
 
 top = tk.Tk()
 top.title('Audio demo')
-top.geometry('1200x800')
+top.geometry('1400x800')
 
 
 labelDetection = tk.Label(top, text = "Pasirinktų garsų detektavimas")
@@ -99,8 +99,7 @@ labelDetection.grid(row=0, column=2)
 
 startingRow = 1
 ###############################################################################
-label_1 = tk.Label(top, text = soundCategories[0])
-label_1.config(font =("Ariel", 12))
+label_1 = tk.Label(top, text = soundCategories[0], font =("Ariel", 12),width=CAT_LABELS_W, anchor="center")
 label_1.grid(row=0+startingRow, column=0)
 
 animalItems = []
@@ -109,65 +108,63 @@ for i in range(10):
     animalItems.append(tk.IntVar())
     
 for i in range(10):
-    cb1 = tk.Checkbutton(top, text=animalsStr[i], variable=animalItems[i], command=Item_test, anchor="w")
+    cb1 = tk.Checkbutton(top, text=animalsStr[i], variable=animalItems[i], command=Item_test, anchor="w", width=CHECK_BOX_W)
     cb1.grid(row=i+1+startingRow, column=0)
 
 ###############################################################################
-label_2 = tk.Label(top, text = soundCategories[1])
-label_2.config(font =("Ariel", 12))
+label_2 = tk.Label(top, text = soundCategories[1], font =("Ariel", 12),width=CAT_LABELS_W, anchor="center")
 label_2.grid(row=0+startingRow, column=1)
 
 natureItems = [] 
 for i in range(10):
     natureItems.append(tk.IntVar())
 for i in range(10):
-    cb1 = tk.Checkbutton(top, text=natureWaterStr[i], variable=natureItems[i], command=Item_test, anchor="w")
+    cb1 = tk.Checkbutton(top, text=natureWaterStr[i], variable=natureItems[i], width=CHECK_BOX_W, anchor="w", command=Item_test)
     cb1.grid(row=i+1+startingRow, column=1)
 
 
 ###############################################################################
-label_3 = tk.Label(top, text = soundCategories[2])
-label_3.config(font =("Ariel", 12))
+label_3 = tk.Label(top, text = soundCategories[2], font =("Ariel", 12),width=CAT_LABELS_W, anchor="center")
 label_3.grid(row=0+startingRow, column=2)
 
 humanItems = [] 
 for i in range(10):
     humanItems.append(tk.IntVar())
 for i in range(10):
-    cb1 = tk.Checkbutton(top, text=humanNon_speechStr[i], variable=humanItems[i], command=Item_test, anchor="w")
+    cb1 = tk.Checkbutton(top, text=humanNon_speechStr[i], variable=humanItems[i], command=Item_test, anchor="w", width=CHECK_BOX_W)
     cb1.grid(row=i+1+startingRow, column=2)
     
     
     
 ###############################################################################
-label_4 = tk.Label(top, text = soundCategories[3])
-label_4.config(font =("Ariel", 12))
+label_4 = tk.Label(top, text = soundCategories[3], font =("Ariel", 12),width=CAT_LABELS_W, anchor="center")
 label_4.grid(row=0+startingRow, column=3)
 
 domesticItems = [] 
 for i in range(10):
     domesticItems.append(tk.IntVar())
 for i in range(10):
-    cb1 = tk.Checkbutton(top, text=buitiniaiStr[i], variable=domesticItems[i], command=Item_test, anchor="w")
+    cb1 = tk.Checkbutton(top, text=buitiniaiStr[i], variable=domesticItems[i], command=Item_test, anchor="w", width=CHECK_BOX_W)
     cb1.grid(row=i+1+startingRow, column=3)
     
 ###############################################################################
-label_5 = tk.Label(top, text = soundCategories[4])
-label_5.config(font =("Ariel", 12))
+label_5 = tk.Label(top, text = soundCategories[4], font =("Ariel", 12),width=CAT_LABELS_W, anchor="center")
 label_5.grid(row=0+startingRow, column=4)
 
 urbanItems = [] 
 for i in range(10):
     urbanItems.append(tk.IntVar())
 for i in range(10):
-    cb1 = tk.Checkbutton(top, text=miestoGarsaiStr[i], variable=urbanItems[i], command=Item_test, anchor="w")
+    cb1 = tk.Checkbutton(top, text=miestoGarsaiStr[i], variable=urbanItems[i], command=Item_test, anchor="w", width=CHECK_BOX_W)
     cb1.grid(row=i+1+startingRow, column=4)
 ###############################################################################
 endRow = startingRow + 11
     
 ################### Detection  ################################################
-probLabel = tk.Label(top, text="Mažiausia tikimybė:")
-probEntry = tk.Entry(top)
+probLabel = tk.Label(top, text="Mažiausia tikimybė (procentais):")
+probValue = tk.StringVar(top, value='70')
+probEntry = tk.Entry(top, textvariable=probValue)
+
 
 probLabel.grid(row=endRow, column=0)
 probEntry.grid(row=endRow, column=1)
@@ -175,8 +172,8 @@ probEntry.grid(row=endRow, column=1)
 button1 = tk.Button(top, text="Detektuoti", height = 6, width = 20, command=Item_test)
 button1.grid(row=endRow+1, column=0)
 
-detectStateLabel = tk.Label(top, text="Detektavimo būsena:")
-inputStateLabel = tk.Label(top, text="nepradėta", bg="yellow") # neaptiktas aptiktas
+detectStateLabel = tk.Label(top, text="Detektavimo būsena:", font =("Ariel", 12))
+inputStateLabel = tk.Label(top, text="nepradėta", bg="yellow", font =("Ariel", 12)) # neaptiktas aptiktas
 # inputStateLabel.config(bg="yellow")
 detectStateLabel.grid(row=endRow+1, column=2)
 inputStateLabel.grid(row=endRow+1, column=3)
@@ -192,17 +189,17 @@ clasfRow += 1
 btnClasf = tk.Button(top, text="Klasifikuoti", height = 6, width = 20, command=Item_test)
 btnClasf.grid(row=clasfRow, column=0)
 
-soundClss_lb = tk.Label(top, text="Garso klasė:")
+soundClss_lb = tk.Label(top, text="Garso klasė:", font =("Ariel", 12))
 soundClss_lb.grid(row=clasfRow, column=1)
 
-currentSoundClass_lb = tk.Label(top, text="------")
+currentSoundClass_lb = tk.Label(top, text="------", font =("Ariel", 12))
 currentSoundClass_lb.grid(row=clasfRow, column=2)
 
-clasfProb_lb = tk.Label(top, text="Tikimybė")
+clasfProb_lb = tk.Label(top, text="Tikimybė", font =("Ariel", 12))
 clasfProb_lb.grid(row=clasfRow, column=3)
 
-currentClasfProb_lb = tk.Label(top, text="0%")
-currentClasfProb_lb.grid(row=clasfRow, column=5)
+currentClasfProb_lb = tk.Label(top, text="0%", font =("Ariel", 12))
+currentClasfProb_lb.grid(row=clasfRow, column=4)
 
 
 top.mainloop()
